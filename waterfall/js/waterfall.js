7fe3f24536bd2dbd64d5b5e5fc96e887f75843c1
flyer153 <flyer153@gmail.com>
2015-01-22 18:55:40 +0800

    add waterfall

---
+++ waterfall/js/waterfall.js
@@ -0,0 +1,14 @@
+/**
+ * 
+ * @authors flyer (flyer153@gmail.com)
+ * @date    2015-01-22 18:36:37
+ * @version $Id$
+ */
+(function(){
+	var waterFall = function(conf) {
+
+	};
+	waterFall.prototype.prepend = function(){
+
+	}
+})();
